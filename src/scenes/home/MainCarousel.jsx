af1b8f13313795f1abca32f6ca84d7375917b132
harsh2077 <110387163+harsh2077@users.noreply.github.com>
2024-04-01 18:57:15 +0530

    Update MainCarousel.jsx

--- src/scenes/home/MainCarousel.jsx
+++ src/scenes/home/MainCarousel.jsx
@@ -4,7 +4,6 @@ import "react-responsive-carousel/lib/styles/carousel.min.css";
 import NavigateBeforeIcon from "@mui/icons-material/NavigateBefore";
 import NavigateNextIcon from "@mui/icons-material/NavigateNext";
 import { shades } from "../../theme";
-
 // imports all images from assets folder
 const importAll = (r) =>
   r.keys().reduce((acc, item) => {
