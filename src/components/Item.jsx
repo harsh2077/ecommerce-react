bcf576a14d7fa3af80401baa4fdf1a309bf02311
harsh2077 <110387163+harsh2077@users.noreply.github.com>
2024-06-01 20:55:46 +0530

    Update Item.jsx

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -2,7 +2,6 @@ import { useState } from "react";
 import { useDispatch } from "react-redux";
 import { IconButton, Box, Typography, useTheme, Button } from "@mui/material";
 import AddIcon from "@mui/icons-material/Add";
-import RemoveIcon from "@mui/icons-material/Remove";
 import { addToCart } from "../state";
 import { useNavigate } from "react-router-dom";
 
