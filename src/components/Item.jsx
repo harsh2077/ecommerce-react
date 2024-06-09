bb91a2a7744c422ecda1dec67d2128c4424f5ee1
harsh2077 <110387163+harsh2077@users.noreply.github.com>
2024-06-09 07:58:24 +0530

    Update Item.jsx

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -25,14 +25,6 @@ const Item = ({ item, width }) => {
         onMouseOver={() => setIsHovered(true)}
         onMouseOut={() => setIsHovered(false)}
       >
-        <img
-          alt={item.name}
-          width="300px"
-          height="400px"
-          src={`http://localhost:1337${url}`}
-          onClick={() => navigate(`/item/${item.id}`)}
-          style={{ cursor: "pointer" }}
-        />
         <Box
           display={isHovered ? "block" : "none"}
           position="absolute"
